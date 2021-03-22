d5c1a4eb40ba48849a43e538bc2a24972a663418
tph01198 <tph01198@naver.com>
2021-03-22 16:52:09 +0900

    version12
    add login_page

---
+++ function.js
@@ -0,0 +1,17 @@
+function clkLogin(){
+  var txtidElem = document.getElementById('txtid');
+  console.log(txtidElem.value);
+  if(txtidElem.value ===''){
+      alert('아이디가 없습니다.');
+      return;
+  }
+
+
+  var txtpwElem = document.getElementById('txtpw');
+  if(txtpwElem.value ===''){
+      alert('비밀번호 없습니다.');
+      return;
+  }
+  location.href = 'PRACTICE.html';
+}
+
