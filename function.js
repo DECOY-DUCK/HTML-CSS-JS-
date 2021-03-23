45383f08e9944a6bee10c439cfb384c8f63f363c
tph01198 <tph01198@naver.com>
2021-03-23 18:19:23 +0900

    version13-1
    add social login frame

--- function.js
+++ function.js
@@ -10,6 +10,7 @@ function clkLogin(){
   var txtpwElem = document.getElementById('txtpw');
   if(txtpwElem.value ===''){
       alert('비밀번호 없습니다.');
+      
       return;
   }
   location.href = 'PRACTICE.html';
